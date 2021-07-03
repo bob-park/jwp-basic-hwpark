ca18bda11ae775a992547310f81dbbcbf9dd1b1f
phw11 <hwpark11@kakao.com>
2021-07-03 16:46:55 +0900

    refactor : log 에 request mapping 추가 시 기록되도록 수정

--- src/main/java/core/mvc/RequestMapping.java
+++ src/main/java/core/mvc/RequestMapping.java
@@ -1,10 +1,15 @@
 package core.mvc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class RequestMapping {
+
+  private final Logger logger = LoggerFactory.getLogger(getClass());
 
   private final Controller forward = new ForwardController();
 
@@ -12,6 +17,9 @@ public class RequestMapping {
 
   public RequestMapping add(String uri, Controller controller) {
     mappings.put(uri, controller);
+
+    logger.info("request mappings [{}]", uri);
+
     return this;
   }
 
