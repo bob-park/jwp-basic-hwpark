c1f11cc767b30f1a81c3b4d7f0d6ba10670032ee
phw11 <hwpark11@kakao.com>
2021-07-03 17:23:17 +0900

    feat : 페이지에 답변달기 기능 추가 하기

--- webapp/js/scripts.js
+++ webapp/js/scripts.js
@@ -1,3 +1,14 @@
+String.prototype.format = function () {
+    const args = arguments;
+    return this.replace(/{(\d+)}/g, function (match, number) {
+        return typeof args[number] != 'undefined'
+            ? args[number]
+            : match
+            ;
+    });
+};
+
+
 $(document).ready(function () {/* jQuery toggle layout */
     $('#btnToggle').click(function () {
         if ($(this).hasClass('on')) {
@@ -8,24 +19,33 @@ $(document).ready(function () {/* jQuery toggle layout */
             $(this).addClass('on');
         }
     });
+
+    /**
+     * handle
+     */
+    // add answer handle
+    $(".answerWrite input[type=submit]").click(addAnswer);
+
+    function addAnswer(e) {
+        e.preventDefault();
+        const queryString = $("form[name=answer]").serialize();
+
+        $.ajax({
+            type: 'post',
+            url: '/api/qna/addAnswer',
+            data: queryString,
+            dataType: 'json',
+            error: (err) => console.error(err),
+            success: onSuccess,
+        });
+    }
+
+    function onSuccess(json, status) {
+        const answerTemplate = $("#answerTemplate").html();
+        const template = answerTemplate.format(json.writer, new Date(json.createdDate), json.contents, json.answerId);
+
+        $(".qna-comment-slipp-articles").prepend(template);
+    }
 });
 
-/**
- * handle
- */
-// add answer handle
-$(".answerWrite input[type=submit]").click(addAnswer);
 
-function addAnswer(e) {
-    e.preventDefault();
-    const queryString = $("form[name=answer]").serialize();
-
-    $.ajax({
-        type: 'post',
-        url: '/api/qna/addAnswer',
-        data: queryString,
-        dataType: 'json',
-        error: (err) => console.error(err),
-        success: (res) => console.log(res),
-    });
-}
