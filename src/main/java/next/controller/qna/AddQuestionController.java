118b996275460077f35092d4d7232801682ef560
phw11 <hwpark11@kakao.com>
2021-07-10 13:55:44 +0900

    feat : 작성자를 파라미터가 아닌 로그인한 사용자가 되도록 기능 추가

--- src/main/java/next/controller/qna/AddQuestionController.java
+++ src/main/java/next/controller/qna/AddQuestionController.java
@@ -6,13 +6,19 @@ import next.controller.UserSessionUtils;
 import next.dao.QuestionDao;
 import next.model.Question;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+import static org.apache.commons.lang3.ObjectUtils.isEmpty;
+
 public class AddQuestionController extends AbstractController {
+
+  private final Logger logger = LoggerFactory.getLogger(getClass());
 
   private final QuestionDao questionDao;
 
@@ -35,11 +41,18 @@ public class AddQuestionController extends AbstractController {
       return jspView("/users/login.jsp");
     }
 
-    questionDao.insert(
-        new Question(
-            request.getParameter("writer"),
-            request.getParameter("title"),
-            request.getParameter("contents")));
+    var user = UserSessionUtils.getUserFromSession(session);
+
+    if (isEmpty(user)) {
+      throw new IllegalStateException("Not logged in.");
+    }
+
+    var newQuestion =
+        questionDao.insert(
+            new Question(
+                user.getUserId(), request.getParameter("title"), request.getParameter("contents")));
+
+    logger.debug("question : {}", newQuestion);
 
     return jspView("/");
   }
